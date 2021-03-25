176022e7c2ee9f01aa69ae96ad2cb94f1703475f
Johnny <jaani.matharu@gmail.com>
2021-03-25 15:23:24 -0400

    added basic structure and testing time functions

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+console.log("DateTime.now()");
+
+//Jquery only used for updated version
+
+//Bootstrap hints: <h4 class="card-header bg-dark text-light">, check bootstrap site
+//After color has been changed it could be linked back by using Jquery, pref JS
+
+//var repeater;
+
+//function showcurrenttime()
+//{
+  //  $('.crnttym').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    //repeater = setTimeout(showcurrenttime, 500);
+
+    //if ($('.interviewtime').data('time') < moment().format('YYYYMMDDHHmmss'))
+    //{
+     //   $(this).parent().addClass('dngr');
+    //}       
+//}
+//showcurrenttime(); 
+
+//public class MyBean {
+    //private Clock clock;  // dependency inject
+   // ...
+    //public void process(LocalDate eventDate) {
+      //if (eventDate.isBefore(LocalDate.now(clock)) {
+    //console.log(clock);
+     // }
+    //}
+  // 
